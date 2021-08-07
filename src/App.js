9a4ab4e398ee0d8f3d2b4ee7d362ecfc5828d61f
Vijay Menon <vijay@Vijays-MacBook-Pro.local>
2021-08-06 17:53:24 -0700

    Basic Example

--- src/App.js
+++ src/App.js
@@ -1,23 +1,36 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from "react";
+import "./App.css";
+
+const Form = () => {
+  console.log("start me up");
+  const [text, setText] = useState(window.localStorage.getItem("text") || "");
+  console.log(text);
+  //without a dependency list, this will get called again every time the state of the app changes
+  //we don't want that because we only want this to run when the state for this component
+  //changes
+  useEffect(() => {
+    window.localStorage.setItem("text", text);
+  });
+
+  const handleFormChange = (e) => {
+    setText(e.target.value);
+  };
+  return (
+    <form>
+      <h3>Write something</h3>
+      <input type="text" value={text} onChange={handleFormChange} />
+      <p>This is the output {text}</p>
+    </form>
+  );
+};
 
 function App() {
+  const [num, setNum] = useState(0);
+  const handler = () => setNum((previous) => previous + 1);
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Form />
+      <button onClick={handler}>{num}</button>
     </div>
   );
 }
